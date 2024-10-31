82ebfefd635b84fd8cb2d8ec94ff13c84bda0b21
Anastasia <karpovanastiya@mail.ru>
2024-10-31 19:26:53 +0300

    LOGIN

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
+CONST CHAR LOGIN_INVITATION[] = "Введите имя пользователя";
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -11,29 +12,43 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)LOGIN_INVITATION);
 	}
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
-		case IDC_BUTTON_Copy:
+		case IDC_EDIT_LOGIN:
 		{
-			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_Password);
-			//Функция GetDlgItem() возвращает HWND дочернего элемента окна по его ResourceID.
-			//HWND - Handler to Window (Дискриптор окна) - это число при помощи которого можно обратиться к окну
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE] = {};
+			SendMessage((HWND)lParam, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, LOGIN_INVITATION) == 0)
+				SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)"");
+			if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(sz_buffer,"") == 0)
+				SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)LOGIN_INVITATION);
+		}
+		case IDC_BUTTON_Copy:
+		{
+			/*HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_Password);*/
+			//Функция GetDlgItem() возвращает HWND дочернего элемента окна по его ResourceID.
+			//HWND - Handler to Window (Дискриптор окна) - это число при помощи которого можно обратиться к окну
+			/*CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_Copy), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);*/
+			/*SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_Copy), WM_SETTEXT, 0, (LPARAM)sz_buffer);*/
+
 		}
 		break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК!", "Info", MB_OK | MB_ICONINFORMATION); break;
