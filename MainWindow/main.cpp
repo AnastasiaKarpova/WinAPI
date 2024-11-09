d08e9f47d6a2a88c3e157504de6a19e47155077c
Anastasia <karpovanastiya@mail.ru>
2024-11-09 20:36:56 +0300

    Window Size & Position DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -35,11 +35,18 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	}
 
 	//2. Создание окна:
-	INT width = GetSystemMetrics(SM_CXSCREEN);
+	/*INT width = GetSystemMetrics(SM_CXSCREEN);
 	INT height = GetSystemMetrics(SM_CYSCREEN);
 	INT w_width = (width - (width * 3/4)) / 2;
-	INT w_height = (height - (height * 3/4)) / 2;
+	INT w_height = (height - (height * 3/4)) / 2;*/
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
 	
+	INT window_width = screen_width * 3 / 4;
+	INT window_height = screen_height * .75;
+	INT window_start_x = screen_width / 8; 
+	INT window_start_y = screen_width / 8;
+
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,   //ExStyles
@@ -48,8 +55,10 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		WS_OVERLAPPEDWINDOW,    //Window style
 		//CW_USEDEFAULT, CW_USEDEFAULT, //Window position
 		//CW_USEDEFAULT, CW_USEDEFAULT, //Window size
-		w_width, w_height,
-		width, height,
+		//w_width, w_height,
+		//width, height,
+		window_start_x, window_start_y,
+		window_width, window_height,
 		NULL,    //Parent Window
 		NULL,    //Main menu ResourceID for MainWindow or RecousceID foe ChildWindow
 		hInstance,
@@ -77,19 +86,47 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		break;
-	case WM_SIZE: 
 	case WM_MOVE:
+	case WM_SIZE:
 	{
-		RECT rc;
-		GetWindowRect(hwnd, &rc);
+		/*RECT rc;
+		GetWindowRect(hwnd, &rc);*/
 		//int xPos = (GetSystemMetrics(SM_CXSCREEN) - rc.right) / 2;
 		//int yPos = (GetSystemMetrics(SM_CYSCREEN) - rc.bottom) / 2;
 		//SetWindowPos(hwnd, 0, xPos, yPos, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
-		int xPos = rc.left;
+		/*int xPos = rc.left;
 		int yPos = rc.top;
 		char sz_title[256];
 		sprintf(sz_title, "Координаты: (%i, %i)", xPos, yPos);
-		SetWindowText(hwnd, sz_title);
+		SetWindowText(hwnd, sz_title);*/
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		INT window_width = rect.right - rect.left;
+		INT window_height = rect.bottom - rect.top;
+		
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+		sprintf
+		(
+			sz_title, 
+			"%s - Position:%ix%i; \tSize: %ix%i", 
+			g_sz_MY_WINDOW_CLASS, 
+			rect.left, rect.top,
+			window_width, window_height
+		);
+
+		/*		Функция sprintf(dst, src, parameters); форматирует src-строку, и результат форматирования сохраняет в dst-строку.
+		dst - Destination (строка получатель);
+		src - Source (строка источник). Как правило является форматированной строкой.
+			  Форматированная строка как правило содержит спецификаторы,
+			  которые подменяются соответствуюшими параметрами.
+				%i - на место этого спецификатора вставляется значение типа 'int'
+				%s - на место этого спецификатора вставляется значение типа 'char*'
+		s... - string;
+		...f - format;
+		printf();	// Print formatted string (вывести на экран отформатированную строку).
+		*/
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
 	}
 		break;
 	case WM_COMMAND:
