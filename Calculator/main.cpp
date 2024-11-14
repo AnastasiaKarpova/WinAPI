af753cc0de3a0820c2b0e5ccb06e71392e989740
Anastasia <karpovanastiya@mail.ru>
2024-11-14 22:08:30 +0300

    Digit buttons DONE

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -2,46 +2,126 @@
 #include<cstdio>
 #include"resource.h"
 
-CONST CHAR g_sz_CALC[] = "Calculator";
+//CONST CHAR g_sz_CALC[] = "Calculator";
+CONST CHAR g_sz_CLASS_NAME[] = "Calc PV_319";
+
+CONST INT g_i_BUTTON_SIZE = 50;
+CONST INT g_i_INTERVAL = 5;
+
+CONST INT g_i_DISPLAY_WIDTH = 384;
+CONST INT g_i_DISPLAY_HEIGHT = 22;
+
+CONST INT g_i_START_X = 10;
+CONST INT g_i_START_Y = 10;
+CONST INT g_i_BUTTON_START_X = g_i_START_X;
+CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL;
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	WNDCLASSEX wc;
-	ZeroMemory(&wc, sizeof(wc));
+	//1.Регистрация класса окна
+	//WNDCLASSEX wc;
+	WNDCLASSEX wClass;
+	ZeroMemory(&wClass, sizeof(wClass));
 
-	wc.style = 0;
-	wc.cbSize = sizeof(wc);
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
+	wClass.style = 0;
+	wClass.cbSize = sizeof(wClass);
+	wClass.cbClsExtra = 0;
+	wClass.cbWndExtra = 0;
 
-	wc.hIcon = (HICON)LoadImage(hInstance, "calculator.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	//wClass.hIcon = (HICON)LoadImage(hInstance, "calculator.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	//wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wClass.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
-	wc.hInstance = hInstance;
-	wc.lpszMenuName = NULL;
-	wc.lpszClassName = g_sz_CALC;
-	wc.lpfnWndProc = (WNDPROC)WndProc;
+	wClass.hInstance = hInstance;
+	wClass.lpszMenuName = NULL;
+	wClass.lpszClassName = g_sz_CLASS_NAME;
+	wClass.lpfnWndProc = (WNDPROC)WndProc;
 
-	if (!RegisterClassEx(&wc))
+	if (!RegisterClassEx(&wClass))
 	{
 		MessageBox(NULL, "Class registreator faild", NULL, MB_OK | MB_ICONERROR);
 		return 0;
 	}
-
+	//2. Создание окна:
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,
-		g_sz_CALC,
-		g_sz_CALC,
+		g_sz_CLASS_NAME,
+		g_sz_CLASS_NAME,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
-		350, 550,
+		//CW_USEDEFAULT, CW_USEDEFAULT,
+		420, 300,
 		NULL,
 		NULL,
 		hInstance,
 		NULL
 	);
-
+	ShowWindow(hwnd, nCmdShow); //Задает режим отображения окна 
+								//(Развернуто на весь экран, свернуто в окноб свернуто в панель задач)
+	UpdateWindow(hwnd);         //Прорисовывает окно
+	//3. Запуск цикла сообщений:
+	MSG msg;   //Создаем сообщение
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (INT)msg.wParam;
+}
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+	{
+		HWND hEdit = CreateWindowEx
+		(
+			NULL,"Edit","",
+			WS_CHILD | WS_VISIBLE | WS_BORDER,
+			10, 10,
+			384, 22,
+			hwnd,
+			(HMENU)IDC_EDIT_DISPLAY,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CHAR sz_digit[2] = "0";
+		for (int i = 6; i >= 0; i -= 3)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				sz_digit[0] = 49 + i + j; //49 - это ASCII код единицы
+				CreateWindowEx
+				(
+					NULL, "Button", sz_digit,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					g_i_BUTTON_START_X + j * (g_i_BUTTON_SIZE + g_i_INTERVAL), 
+					g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL)* (2 - i / 3),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)IDC_BUTTON_1 + i + j,
+					GetModuleHandle(NULL),
+					NULL 
+				);
+			}
+		}
+	}
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
 }
