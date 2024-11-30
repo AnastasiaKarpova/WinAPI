20d31bc7e032d47dfd9fdbd96ef0b25cde59ec7e
Anastasia <karpovanastiya@mail.ru>
2024-11-30 21:04:06 +0300

    Color bug FIXED

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -522,8 +522,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CONTEXTMENU:
 	{
 		HMENU hSubMenu = CreatePopupMenu();
-		InsertMenu(hSubMenu, 0, MF_BYPOSITION | MF_STRING, IDR_SQUARE_BLUE, "Square Blue"); 
-		InsertMenu(hSubMenu, 0, MF_BYPOSITION | MF_STRING, IDR_METAL_MISTRAL, "Metal Mistral"); 
+		InsertMenu(hSubMenu, 0, MF_BYPOSITION | MF_STRING, IDR_METAL_MISTRAL, "Metal Mistral");
+		InsertMenu(hSubMenu, 0, MF_BYPOSITION | MF_STRING, IDR_SQUARE_BLUE, "Square Blue");
 		
 		HMENU hMenu = CreatePopupMenu();
 		InsertMenu(hMenu, 1, MF_BYPOSITION | MF_POPUP, (UINT_PTR)hSubMenu, "Topic");
@@ -557,15 +557,18 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		DestroyMenu(hSubMenu); 
 		DestroyMenu(hMenu);
 
-		color_index = skin_index - IDR_CONTEXT_MENU - 1;
-		HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
-		HDC hdcDisplay = GetDC(hEditDisplay);
-		SendMessage(hwnd, WM_CTLCOLOREDIT, (WPARAM)hdcDisplay, (LPARAM)hEditDisplay);
-		ReleaseDC(hEditDisplay, hdcDisplay);
+		if(skin_index >= IDR_SQUARE_BLUE && skin_index <= IDR_METAL_MISTRAL)
+		{
+			color_index = skin_index - IDR_CONTEXT_MENU - 1;
+			HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			HDC hdcDisplay = GetDC(hEditDisplay);
+			SendMessage(hwnd, WM_CTLCOLOREDIT, (WPARAM)hdcDisplay, (LPARAM)hEditDisplay);
+			ReleaseDC(hEditDisplay, hdcDisplay);
 
-		CHAR sz_buffer[MAX_PATH]{};
-		SendMessage(hEditDisplay, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
-		SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			CHAR sz_buffer[MAX_PATH]{};
+			SendMessage(hEditDisplay, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
 	}
 	break;
 	case WM_DESTROY:
