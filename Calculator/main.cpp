b52e3054312cebd68403c7aaab21570b95b6cda2
Anastasia <karpovanastiya@mail.ru>
2024-12-03 22:03:55 +0300

    Resource-only DLL DONE

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -36,6 +36,7 @@ CONST COLORREF g_WINDOW_BACKGROUND[] = {RGB(0, 0, 150), RGB(75, 75, 75), RGB(0, 
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 VOID SetSkin(HWND hwnd, CONST CHAR* skin);
+VOID SetSkinFromDLL(HWND hwnd, CONST CHAR* skin);
 
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -554,11 +555,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 		switch (skin_index)
 		{
-		case IDR_SQUARE_BLUE: SetSkin(hwnd, "square_blue"); break;
-		case IDR_METAL_MISTRAL: SetSkin(hwnd, "metal_mistral"); break;
-		case IDR_SQUARE_ORANGE: SetSkin(hwnd, "square_orange"); break;
-		case IDR_SQUARE_LIGHT_BLUE: SetSkin(hwnd, "square_light_blue"); break; 
-		case IDR_SQUARE_LILAC: SetSkin(hwnd, "square_lilac"); break; 
+		case IDR_SQUARE_BLUE: SetSkinFromDLL(hwnd, "square_blue"); break;
+		case IDR_METAL_MISTRAL: SetSkinFromDLL(hwnd, "metal_mistral"); break;
+		case IDR_SQUARE_ORANGE: SetSkinFromDLL(hwnd, "square_orange"); break;
+		case IDR_SQUARE_LIGHT_BLUE: SetSkinFromDLL(hwnd, "square_light_blue"); break;
+		case IDR_SQUARE_LILAC: SetSkinFromDLL(hwnd, "square_lilac"); break;
 		case IDR_EXIT: DestroyWindow(hwnd);/* break;*/
 		}
 		
@@ -643,4 +644,25 @@ VOID SetSkin(HWND hwnd, CONST CHAR* skin)
 
 		);*/
 	}
+}
+
+VOID SetSkinFromDLL(HWND hwnd, CONST CHAR* skin)
+{
+	CHAR filename[MAX_PATH]{};
+	sprintf(filename, "ButtonsBMP\\%s", skin);
+	HMODULE hInst = LoadLibrary(filename);
+	for (int i = IDC_BUTTON_0; i <= IDC_BUTTON_EQUAL; i++)
+	{
+		HBITMAP buttonBMP = (HBITMAP)LoadImage
+		(
+			hInst,
+			MAKEINTRESOURCE(i),
+			IMAGE_BITMAP,
+			i > IDC_BUTTON_0 ? g_i_BUTTON_SIZE : g_i_BUTTON_DOUBLE_SIZE, 
+			i < IDC_BUTTON_EQUAL ? g_i_BUTTON_SIZE : g_i_BUTTON_DOUBLE_SIZE,
+			NULL
+		);
+		SendMessage(GetDlgItem(hwnd, i), BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)buttonBMP);
+	}
+	FreeLibrary(hInst);
 }
